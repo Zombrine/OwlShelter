8a08f8b46f4732d31410cb5aabc94c5b2274a64a
mdkChaos <mdkchaos1@gmail.com>
2017-09-15 18:14:53 +0200

    Upload to github, part 1

---
+++ Descriptions/2D.js
@@ -0,0 +1,38 @@
+var Info = function () {
+   this.Show = function ()
+   {
+      $('#Description').append ("\
+      <h3>------------------------------   2д арт   ------------------------------</h3>\
+<b>Ярослава</b><br>\
+<img src='img/team/2d/yaroslava.jpg' ><br>\
+<br>\
+<b>Александра</b><br>\
+<img src='img/team/2d/alexandra.jpg?dummy=$randomForImg' ><br>\
+<br>\
+<b>Алексей Кузьмик</b><br>\
+<img src='img/team/2d/alexeiKyzmik.jpg?dummy=$randomForImg' ><br>\
+<br>\
+<b>Лера</b>  <a href='http://illustrators.ru/users/lera-flomastery' target='_blank'>Портфолио</a><br>\
+<img src='img/team/2d/lera.jpg?dummy=$randomForImg' ><br>\
+<br>\
+<b>Майкл Сумин</b><br>\
+<img src='img/team/2d/maiklSumin.jpg?dummy=$randomForImg' ><br>\
+<br>\
+<b>Руслана Холина</b><br>\
+<img src='img/team/2d/ruslanaHolina.jpg?dummy=$randomForImg' ><br>\
+<br>\
+<b>Юлия Войтенко</b><br>\
+<img src='img/team/2d/uliaVoitenko.jpg?dummy=$randomForImg' ><br>\
+<br>\
+<b>Валерий Sinimus</b><br>\
+<img src='img/team/2d/valeriiSinimus.jpg?dummy=$randomForImg' ><br>\
+<br>\
+<b>Виктория Черненко</b><br>\
+<a href='https://www.behance.net/victoritor3738' target='_blank'> 2д портфолио</a><br>\
+<img src='img/team/2d/viktoriaChernenko.jpg?dummy=$randomForImg' ><br>\
+<br>\
+<b>Рада Слюта</b><br>\
+<img src='img/team/2d/radaSliuta.jpg?dummy=$randomForImg' ><br>\
+      ");
+   }
+}
---
+++ Descriptions/3D.js
@@ -0,0 +1,33 @@
+var Info = function () {
+   this.Show = function ()
+   {
+      $('#Description').append ("\
+      <h3>------------------------------   3д арт   ------------------------------</h3>\
+<b>Александр Анякин</b><br>\
+<img src='img/team/3d/alexandrAnakin.jpg?dummy=$randomForImg' ><br>\
+<br>\
+<b>dfDast </b><br>\
+<img src='img/team/3d/dfDast .jpg?dummy=$randomForImg' ><br>\
+<br>\
+<b>Евгения Шутенко (персонажи)</b><br>\
+<img src='img/team/3d/evgeniaShytanko.jpg?dummy=$randomForImg' ><br>\
+<br>\
+<b>Никита Подобедов</b><br>\
+<img src='img/team/3d/nikitaPobedov?dummy=$randomForImg.jpg' ><br>\
+<br>\
+<b>Виктор</b><br>\
+<img src='img/team/3d/viktor.jpg?dummy=$randomForImg' ><br>\
+<br>\
+<b>Виталий Козюк</b><br>\
+<img src='img/team/3d/vitaliKozyk.jpg?dummy=$randomForImg' ><br>\
+<br>\
+<b>Владислав Попович</b><br>\
+<img src='img/team/3d/vladislavPopovich.jpg?dummy=$randomForImg' ><br>\
+<br>\
+<b>Денис Шевченко </b> &nbsp;&nbsp;&nbsp; <a href='https://sketchfab.com/denishev/models' target='_blank'> Портфолио 3Д </a> <br> \
+&nbsp;&nbsp;&nbsp; \
+  <a href='https://vimeo.com/200985358?utm_source=email&utm_medium=vimeo-cliptranscode-201504&utm_campaign=29220' target='_blank'> Портфолио 3д анимация </a> <br> \
+<img src='img/team/3d/denisShevchenko.jpg?dummy=$randomForImg' ><br> \
+      ");
+   }
+}
